7fd5b327fe98b6714b6844dab19226a20efde448
Zach Tibbitts <zachtib@gmail.com>
2020-06-02 22:25:20 -0400

    home page things

--- timeslots/models.py
+++ timeslots/models.py
@@ -1,10 +1,43 @@
 import uuid
-
 from django.db import models
+from django.db.models import Q
+from datetime import datetime, timedelta
+
+
+class TimeSlotManager(models.Manager):
+
+    def check_for_expired_pendings(self):
+        now = datetime.now()
+        self.filter(
+            status=TimeSlot.Status.PENDING,
+            pending_until__lt=now
+        ).update(
+            status=TimeSlot.Status.AVAILABLE,
+            pending_until=None
+        )
+
+    def all_future(self):
+        today = datetime.today().date()
+        now = datetime.now().time()
+
+        return self.filter(
+            Q(date__gt=today) | Q(date=today, begin_time__gt=now)
+        )
+
+    def all_future_available(self):
+        return self.all_future().filter(status=TimeSlot.Status.AVAILABLE)
+
+    def all_available_for_date(self, date):
+        now = datetime.now().time()
+
+        return self.filter(
+            status=TimeSlot.Status.AVAILABLE,
+            date=date,
+            begin_time__gt=now
+        )
 
 
 class TimeSlot(models.Model):
-
     class Status(models.TextChoices):
         AVAILABLE = 'A', 'Available'
         PENDING = 'P', 'Pending'
@@ -22,3 +55,14 @@ class TimeSlot(models.Model):
     name = models.CharField(max_length=254, null=True, blank=True, default=None)
     email_address = models.EmailField(null=True, blank=True, default=None)
     details = models.TextField(null=True, blank=True, default=None)
+
+    objects = TimeSlotManager()
+
+    def format_range(self):
+        return f'{self.begin_time} - {self.end_time}'
+
+    def set_pending(self):
+        expiration_time = datetime.now() + timedelta(minutes=20)
+
+        self.pending_until = expiration_time
+        self.status = TimeSlot.Status.PENDING
--- timeslots/views.py
+++ timeslots/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from timeslots.models import TimeSlot
+
+
+def home(request):
+    available_times = TimeSlot.objects.all_future_available()
+
+    times_by_date = {}
+    for time in available_times:
+        if time.date in times_by_date:
+            times_by_date[time.date].append(time)
+            times_by_date[time.date].sort(key=lambda item: item.begin_time)
+        else:
+            times_by_date[time.date] = [time]
+
+    print(times_by_date)
+
+    return render(request, 'home.html', {
+        'times_by_date': times_by_date,
+    })
